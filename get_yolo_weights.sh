72eb31e903bf9dafdd8952cd74e5edadde478f4f
JongSuk Choi <cjs0818@ust.ac.kr>
2018-03-27 14:44:35 +0900

    get_yolo_weights.sh modifiled

--- get_yolo_weights.sh
+++ get_yolo_weights.sh
@@ -1,20 +1,15 @@
 # Pre-trained Full Weights
-wget https://pjreddie.com/media/files/yolo.weights -P ../darknet_weights_data
+#wget https://pjreddie.com/media/files/yolo.weights -P ../darknet_weights_data
 wget https://pjreddie.com/media/files/yolo-voc.weights -P ../darknet_weights_data
-wget https://pjreddie.com/media/files/tiny-yolo.weights -P ../darknet_weights_data
-wget https://pjreddie.com/media/files/yolo9000.weights -P ../darknet_weights_data
+#wget https://pjreddie.com/media/files/tiny-yolo.weights -P ../darknet_weights_data
+#wget https://pjreddie.com/media/files/yolo9000.weights -P ../darknet_weights_data
+
 
 # Pre-trained Convolutional Weights for Training
 wget http://pjreddie.com/media/files/darknet19_448.conv.23 -P ../darknet_weights_data
+
 
 # Pre-trained Full Weights for RNN
 #wget https://pjreddie.com/media/files/grrm.weights -P ../darknet_weights_data
 #wget https://pjreddie.com/media/files/shakespeare.weights -P ../darknet_weights_data
 
-
-
-#wget http://pjreddie.com/media/files/darknet19_448.conv.23
-#wget https://pjreddie.com/media/files/yolo.weights
-#wget https://pjreddie.com/media/files/yolo-voc.weights
-#wget https://pjreddie.com/media/files/tiny-yolo.weights
-#wget https://pjreddie.com/media/files/yolo9000.weights
